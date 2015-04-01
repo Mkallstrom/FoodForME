6275c672c278822d1ad0d16b1b1b69d9e3416431
Mkallstrom <martinkallstrom92@gmail.com>
2015-04-01 11:09:18 +0200

    Inventory Activity created

--- Application/app/src/main/java/com/example/martin/foodforme/InventoryActivity.java
+++ Application/app/src/main/java/com/example/martin/foodforme/InventoryActivity.java
@@ -1,26 +1,24 @@
 package com.example.martin.foodforme;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class InventoryActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_inventory);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_inventory, menu);
         return true;
     }
 
@@ -37,9 +35,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-    public void toInventory(View view){
-        Intent intent = new Intent(this, InventoryActivity.class);
-        startActivity(intent);
     }
 }
