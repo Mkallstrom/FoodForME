6d85050aeb0c124719c1505531ba1be1060f6425
warlang <thinker20>
2015-04-17 12:02:51 +0200

    check refrigerator if its empty and say so

--- Application/app/src/main/java/com/example/martin/foodforme/AddProductActivity.java
+++ Application/app/src/main/java/com/example/martin/foodforme/AddProductActivity.java
@@ -3,17 +3,10 @@ package com.example.martin.foodforme;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.res.AssetManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
-import android.media.ExifInterface;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.StrictMode;
-import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -26,19 +19,12 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.googlecode.tesseract.android.TessBaseAPI;
-
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
@@ -92,48 +78,6 @@ public class AddProductActivity extends ActionBarActivity {
         setTitle("Add Product");
         context = this;
 
-        String[] paths = new String[] { DATA_PATH, DATA_PATH + "tessdata/" };
-        for (String path : paths) {
-            File dir = new File(path);
-            if (!dir.exists()) {
-                if (!dir.mkdirs()) {
-                    Log.v(TAG, "ERROR: Creation of directory " + path + " on sdcard failed");
-                    return;
-                } else {
-                    Log.v(TAG, "Created directory " + path + " on sdcard");
-                }
-            }
-
-        }
-        if (!(new File(DATA_PATH + "tessdata/" + lang + ".traineddata")).exists()) {
-            try {
-
-                AssetManager assetManager = getAssets();
-                InputStream in = assetManager.open("tessdata/" + lang + ".traineddata");
-                //GZIPInputStream gin = new GZIPInputStream(in);
-                OutputStream out = new FileOutputStream(DATA_PATH
-                        + "tessdata/" + lang + ".traineddata");
-
-                // Transfer bytes from in to out
-                byte[] buf = new byte[1024];
-                int len;
-                //while ((lenf = gin.read(buff)) > 0) {
-                while ((len = in.read(buf)) > 0) {
-                    out.write(buf, 0, len);
-                }
-                in.close();
-                //gin.close();
-                out.close();
-
-                Log.v(TAG, "Copied " + lang + " traineddata");
-            } catch (IOException e) {
-                Log.e(TAG, "Was unable to copy " + lang + " traineddata " + e.toString());
-            }
-        }
-        _path = DATA_PATH + "/ocr.jpg";
-
-
-
         localBarcodes = getSharedPreferences("localBarcodes", 0);
 
         productName = (EditText)findViewById(R.id.productBox);
@@ -152,7 +96,6 @@ public class AddProductActivity extends ActionBarActivity {
 
             //trying to retrieve data from the source. If there
             //is no connection, this line will fail
-            urlConnect.setConnectTimeout(1000);
             Object objData = urlConnect.getContent();
             CheckBox checkBox = (CheckBox) findViewById(R.id.connectionCheck);
             checkBox.setChecked(true);
@@ -173,7 +116,13 @@ public class AddProductActivity extends ActionBarActivity {
         }
         else
         {
-
+            if(databaseHasProduct) // NOTE: This one seems to be false every time (but product.setText is done in the GetProductDetails AsyncTask)
+            {
+                productName.setText(databaseName);
+            }
+            else {
+                //Nothing found at all!
+            }
         }
 
         // Fills the spinner with years
@@ -184,125 +133,6 @@ public class AddProductActivity extends ActionBarActivity {
 
         // Fills the spinner with days
         fillSpinnerDay();
-    }
-
-    public void startCameraActivity(View view) {
-        File file = new File(_path);
-        Uri outputFileUri = Uri.fromFile(file);
-
-        final Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
-
-        startActivityForResult(intent, 0);
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        Log.i(TAG, "resultCode: " + resultCode);
-
-        if (resultCode == -1) {
-            onPhotoTaken();
-        } else {
-            Log.v(TAG, "User cancelled");
-        }
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        outState.putBoolean(AddProductActivity.PHOTO_TAKEN, _taken);
-    }
-
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        Log.i(TAG, "onRestoreInstanceState()");
-        if (savedInstanceState.getBoolean(AddProductActivity.PHOTO_TAKEN)) {
-            onPhotoTaken();
-        }
-    }
-
-    protected void onPhotoTaken() {
-        _taken = true;
-
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inSampleSize = 4;
-
-        Bitmap bitmap = BitmapFactory.decodeFile(_path, options);
-
-        try {
-            ExifInterface exif = new ExifInterface(_path);
-            int exifOrientation = exif.getAttributeInt(
-                    ExifInterface.TAG_ORIENTATION,
-                    ExifInterface.ORIENTATION_NORMAL);
-
-            Log.v(TAG, "Orient: " + exifOrientation);
-
-            int rotate = 0;
-
-            switch (exifOrientation) {
-                case ExifInterface.ORIENTATION_ROTATE_90:
-                    rotate = 90;
-                    break;
-                case ExifInterface.ORIENTATION_ROTATE_180:
-                    rotate = 180;
-                    break;
-                case ExifInterface.ORIENTATION_ROTATE_270:
-                    rotate = 270;
-                    break;
-            }
-
-            Log.v(TAG, "Rotation: " + rotate);
-
-            if (rotate != 0) {
-
-                // Getting width & height of the given image.
-                int w = bitmap.getWidth();
-                int h = bitmap.getHeight();
-
-                // Setting pre rotate
-                Matrix mtx = new Matrix();
-                mtx.preRotate(rotate);
-
-                // Rotating Bitmap
-                bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false);
-            }
-
-            // Convert to ARGB_8888, required by tess
-            bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
-
-        } catch (IOException e) {
-            Log.e(TAG, "Couldn't correct orientation: " + e.toString());
-        }
-
-        // _image.setImageBitmap( bitmap );
-
-        Log.v(TAG, "Before baseApi");
-
-        TessBaseAPI baseApi = new TessBaseAPI();
-        baseApi.setDebug(true);
-        baseApi.init(DATA_PATH, lang);
-        baseApi.setImage(bitmap);
-
-        String recognizedText = baseApi.getUTF8Text();
-
-        baseApi.end();
-
-        // You now have the text in recognizedText var, you can do anything with it.
-        // We will display a stripped out trimmed alpha-numeric version of it (if lang is eng)
-        // so that garbage doesn't make it to the display.
-
-        Log.v(TAG, "OCRED TEXT: " + recognizedText);
-
-        if ( lang.equalsIgnoreCase("swe") ) {
-            recognizedText = recognizedText.replaceAll("[^a-zA-Z0-9]+", " ");
-        }
-
-        recognizedText = recognizedText.trim();
-
-        if ( recognizedText.length() != 0 ) {
-            Toast.makeText(context, recognizedText, Toast.LENGTH_LONG).show();
-        }
-
-        // Cycle done.
     }
 
     class CreateNewProduct extends AsyncTask<String, String, String> {
