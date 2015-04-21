7f061f6b9b99b0e14ce3f0ba350eb51c625bbe9a
warlang <thinker20>
2015-04-21 10:57:34 +0200

    u
    itemNotfound

---
+++ Application/app/src/main/java/com/example/martin/foodforme/Item_not_found.java
@@ -0,0 +1,53 @@
+package com.example.martin.foodforme;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+
+public class Item_not_found extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_not_found);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_item_not_found, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void getInputName(View view){
+        Intent intent = new Intent();
+        TextView tv = (TextView)findViewById(R.id.productName);
+        intent.putExtra("productName",tv.getText().toString());
+        setResult(RESULT_OK,intent);
+        finish();
+    }
+
+}
