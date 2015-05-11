1bae634cb569e79aa963c84711c46c15a8ea2eb7
warlang <thinker20>
2015-05-11 11:14:49 +0200

    integer lenght

--- Application/app/src/main/java/com/example/martin/foodforme/RequirementActivity.java
+++ Application/app/src/main/java/com/example/martin/foodforme/RequirementActivity.java
@@ -86,10 +86,10 @@ public class RequirementActivity extends ActionBarActivity {
                     .setView(txtUrl)
                     .setPositiveButton("Apply", new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int whichButton) {
-                            int amount = Integer.parseInt(txtUrl.getText().toString());
-                            if(amount>999999999)
+                            int amount = txtUrl.getText().toString().length();
+                            if(amount>9)
                             {
-                                amount = 999999999;
+                                amount = 9;
                             }
                             Product item = requirementList.get(info.position);
                             item.setAmount(Integer.toString(amount));
